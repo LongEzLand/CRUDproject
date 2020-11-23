0f38423e9ccc49db9d8b9d89a21758c04942b5d3
LongEzLand <martinpaju@gmail.com>
2020-11-23 23:26:07 +0200

    Seeing the request body...

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true }))
 
 app.listen(3000, function(){
     console.log('Listening on 3000');
@@ -10,6 +13,6 @@ app.get('/', (req, res) => {
   })
 
 app.post('/quotes', (req, res) => {
-    console.log('Heloooooo World 4!!!!')
+    console.log(req.body);
 })
 console.log('May the Node be with You');
