42d99185f69ad818c86d3616ca87e6944c60f66a
LongEzLand <martinpaju@gmail.com>
2020-11-24 00:29:58 +0200

    Mongo Atlas connection established

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = 'mongodb+srv://Yoda:WIJTw18H7forYhdQ@cluster0.ruwzh.mongodb.net/yodaBase?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, (err, client) => {
+    if (err) return console.error(err);
+  console.log('Connected to Database');
+})
 
 app.use(bodyParser.urlencoded({extended: true }))
+
+
 
 app.listen(3000, function(){
     console.log('Listening on 3000');
 })
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
