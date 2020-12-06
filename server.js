d465babeafaf84cd89bf8cac303b688141728d74
LongEzLand <martinpaju@gmail.com>
2020-12-06 19:24:19 +0200

    moved express req handlers into mongo 'then' call

--- server.js
+++ server.js
@@ -6,25 +6,29 @@ const connectionString = 'mongodb+srv://Yoda:WIJTw18H7forYhdQ@cluster0.ruwzh.mon
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-  })
-  .catch(error => console.error(error))
+    console.log('Connected to Database');
+    
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+    app.use(bodyParser.urlencoded({extended: true }))
 
-app.use(bodyParser.urlencoded({extended: true }))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-
-
+app.post('/quotes', (req, res) => {
+  quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
+})
 app.listen(3000, function(){
     console.log('Listening on 3000');
 })
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-})
 console.log('May the Node be with You');
+    
+  })
+  .catch(error => console.error(error))
+
